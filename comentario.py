b116de9e0fbf8fa521dcdf9a1feade01fe111758
yaritza lopez <160291834+rouuhehe@users.noreply.github.com>
2025-11-12 10:21:02 -0500

    Add files via upload

--- comentario.py
+++ comentario.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 import uuid
 import os
@@ -8,6 +9,7 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    nombre_bucket = os.environ["BUCKET_NAME"]
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -20,7 +22,18 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
+    s3 = boto3.client('s3')
+    s3.put_object(
+        Bucket=nombre_bucket,
+        Key=f"{tenant_id}/{uuidv1}.txt",
+        Body=json.dumps(comentario),
+        ContentType='application/json'
+    )
+
     # Salida (json)
+    print(f"Archivo subido a S3: s3://{nombre_bucket}/{tenant_id}/{uuidv1}.txt")
+
     print(comentario)
     return {
         'statusCode': 200,
